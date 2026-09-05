a07a6bf985e202b883f3bacb1545dd6faa8c2903
Munneth Gill <munnethgill@gmail.com>
2026-09-04 19:18:02 -0700

    Remove occupancy cron job

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,26 +1,14 @@
-import Image from "next/image";
 import { ChartBarLabel } from "@/components/chartProd";
 import Form from "@/components/form";
+import { PacificTime } from "@/components/pacific-time";
 
 export default function Home() {
   return (
     <>
       <div>
         <h1>Occupancy</h1>
-        <p>Check the console for cron job logs</p>
-        <p>
-          West Coast Time:{" "}
-          {new Date().toLocaleString("en-US", {
-            timeZone: "America/Los_Angeles",
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: false,
-          })}
-        </p>
+        <p>Occupancy data is displayed from the database.</p>
+        <p>West Coast Time: <PacificTime /></p>
         <div className="flex gap-6">
           <div className="flex-1">
             <ChartBarLabel />
